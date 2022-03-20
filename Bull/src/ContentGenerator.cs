a3038a2f5b8b61fe84598c1918aed6f350652922
Asko Nomm <asko@bien.ee>
2022-03-20 01:31:30 +0100

    bump

--- Bull/src/ContentGenerator.cs
+++ Bull/src/ContentGenerator.cs
@@ -2,5 +2,13 @@ namespace Bull;
 
 public class ContentGenerator
 {
-    
+    public static List<ContentItem> GenerateItemList(ContentGenerationRequest request)
+    {
+        return new List<ContentItem>();
+    }
+
+    public static List<ContentGroup> GenerateGroupList(ContentGenerationRequest request)
+    {
+        return new List<ContentGroup>();
+    }
 }
---
+++ Bull/src/Records/ContentGroup.cs
@@ -0,0 +1,5 @@
+﻿namespace Bull;
+
+public record ContentGroup
+{
+}
